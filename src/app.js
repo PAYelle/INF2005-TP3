d73aee17364c2dc86096aa21a6133a8dc81fc14d
PAYelle <payelle15@gmial.com>
2018-11-15 17:22:49 -0500

    Ajout app.ja

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-// const API_KEY = "ef39282f7c14259b9b3517f461b88510";
-// const API_URL = 'https://api.themoviedb.org/3';
 //
 // const main = async () => {
 //     const httpResponse = await fetch(`${ API_URL }/trending/movie/week?api_key=${ API_KEY }`);
@@ -16,3 +14,25 @@
 // };
 //
 // document.addEventListener('DOMContentLoaded', main);
+"use strict";
+//CONSTANTES
+const api = {
+    key: "ef39282f7c14259b9b3517f461b88510",
+    url: 'https://api.themoviedb.org/3'
+};
+const url = {
+    TRENDING_FILM: `${api.url}/trending/move/week?api_key=${api.key}`,
+    SEARCH_FILM: 'https://developers.themoviedb.org/3/search/search-movies',
+    DETAILS_FILM: 'https://developers.themoviedb.org/3/movies/get-movie-details',
+    REVIEW_FILM: 'https://developers.themoviedb.org/3/movies/get-movie-reviews'
+};
+
+//FUNCTIONS
+const addItemHtmlDoc = (attribute, item) => document.querySelector(attribute).innerHTML = item;
+const formaterFilm = jsonFilmContent => [jsonFilmContent.title, jsonFilmContent.vote_average];
+const main = async () => {
+    const httpResp = await fetch(url.TRENDING_FILM);
+    const jsonContent = await httpResp.json();
+};
+
+document.addEventListener('DOMContentLoaded', main);
